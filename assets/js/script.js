c1eb541011c4d0f20290113d01955d899c2ff912
Chihyu <chihyu10@gmail.com>
2020-05-29 15:03:39 +0200

    temp push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
-test
+(function () {
+    let num1 = document.getElementById('number1').innerHTML;
+    let num2 = document.getElementById('number2').innerHTML;
+    let num3 = document.getElementById('number3').innerHTML;
+
+    console.log(num1,num2,num3);
+    let current = 0;
+    let interval = 0;
+    const callback = () => {
+        let nextcount = current++;
+        console.log(nextcount);
+        if (nextcount == 9) {
+            clearInterval(interval)
+        }
+    }
+
+    interval = setInterval(callback,50,0);
+
+
+    // function countNumber(){
+    //     let testnumber = document.getElementById("numberSpecific").innerHTML;
+    //     let current = 0;
+    //     let interval = null;
+    //     const callback = () => {
+    //         let nextcount = current++;
+    //         console.log(nextcount);
+    //         if (nextcount == testnumber) {
+    //             clearInterval(interval)
+    //         }
+    //     }
+    //     interval = setInterval(callback,0,1);
+    //
+    //
+    //
+    // }
+})();
+
+
+
+// testnumber.innerHTML = "leylowmo";
+
+
+
+
